3ff024ec0bd2bb5cd81cda92ac5891893ef34ed5
Sk. Zaber Ahmed <zaber0305@gmail.com>
2023-08-09 22:46:18 +0600

    Modify: technician profile page

--- client/src/pages/Dashboard/Profile/index.tsx
+++ client/src/pages/Dashboard/Profile/index.tsx
@@ -1,35 +1,61 @@
-import { Text, Box, HStack, VStack } from '@chakra-ui/react';
+import { Text, Box, Flex, VStack } from '@chakra-ui/react';
 import TechnicianWorkingDays from '../../../components/Technician Working Days';
 import InputTechnician from '../../../components/Input Technician';
+import { timeSlotGenerator } from '../../../utils/timeSlotgenerator';
 
 const TechnicianProfile = () => {
+	const timeSlots = timeSlotGenerator(7, 20); //generating time slots between 7am and 8pm
 	return (
 		<>
-			<HStack spacing={'3rem'}>
-				<VStack
+			<Flex>
+				<Box
 					color={'secondary'}
-					width={'100%'}>
-					<InputTechnician
-						id={'fullName'}
-						isRequired={true}
-						type={'text'}
-						placeholder={'Enter Full Name'}></InputTechnician>
-					{/* <Text>Rakib</Text> */}
-				</VStack>
+					flex={0.5}>
+					Rakib
+				</Box>
+				<Box
+					color={'secondary'}
+					flex={0.5}>
+					<>
+						<Text
+							mb={2}
+							fontFamily={'Inter'}
+							fontWeight={'500'}
+							fontSize={'1.5rem'}>
+							Which days can you work on ?
+						</Text>
+						<TechnicianWorkingDays colorScheme="accent" />
+						<>
+							<Text
+								mt={'2rem'}
+								mb={2}
+								fontFamily={'Inter'}
+								fontWeight={'500'}
+								fontSize={'1.5rem'}>
+								Which are the best times for you ?
+							</Text>
+							<Text
+								mb={2}
+								fontFamily={'Inter'}
+								fontWeight={'500'}
+								fontSize={'1rem'}>
+								{' '}
+								Select the times that work best for you
+							</Text>
 
-				<VStack
-					color="secondary"
-					width={'100%'}>
-					<Text
-						as="b"
-						fontSize={'1.25rem'}
-						fontWeight={'500'}
-						fontFamily={'Inter'}>
-						Which days can you work on ?
-					</Text>
-					<TechnicianWorkingDays colorScheme={'accent'} />
-				</VStack>
-			</HStack>
+							{/* <Flex>
+								{timeSlots.map((slot) => {
+									return (
+										<Flex bg={'accent'}>
+											<Text color={'secondary'}>{slot.slotTime}</Text>
+										</Flex>
+									);
+								})}
+							</Flex> */}
+						</>
+					</>
+				</Box>
+			</Flex>
 		</>
 	);
 };
---
+++ client/src/utils/timeSlotgenerator.ts
@@ -0,0 +1,20 @@
+export interface TimeSlot {
+	slotName: string;
+	slotTime: string;
+}
+export function timeSlotGenerator(start: number, end: number) {
+	const timeSlots: TimeSlot[] = [];
+	for (let hour = 7; hour <= 20; hour++) {
+		const slotStartTime = `${hour.toString().padStart(2, '0')}:00`;
+		const slotEndTime = `${(hour + 1).toString().padStart(2, '0')}:00`;
+		const slotName = String.fromCharCode(65 + hour - 7); // A, B, C, ...
+		const slotTime = `${slotStartTime}-${slotEndTime}`;
+
+		timeSlots.push({
+			slotName: slotName,
+			slotTime: slotTime,
+		});
+	}
+
+	return timeSlots;
+}
