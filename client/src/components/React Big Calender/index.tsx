058e54ac1d23f135604bef8c2bbc2b71c9d59c50
Sk. Zaber Ahmed <zaber0305@gmail.com>
2023-08-13 13:48:36 +0600

    Add: Styles for calendar

--- client/src/components/React Big Calender/index.tsx
+++ client/src/components/React Big Calender/index.tsx
@@ -1,7 +1,7 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import './calendar.styles.css';
 // import './../../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
-import './../../../node_modules/react-big-calendar/lib/sass/styles';
 
 const localizer = momentLocalizer(moment);
 
@@ -11,19 +11,44 @@ const myEventsList = [
 		end: new Date(),
 		title: 'Sample Event Zaber',
 	},
+	{
+		start: moment(new Date()).add(1, 'days').toDate(),
+		end: moment(new Date()).add(1, 'days').toDate(),
+		title: 'Sample Event 2',
+	},
+	{
+		start: moment(new Date()).add(2, 'days').toDate(),
+		end: moment(new Date()).add(2, 'days').toDate(),
+		title: 'Sample Event 3',
+	},
+	{
+		start: moment(new Date()).add(3, 'days').toDate(),
+		end: moment(new Date()).add(3, 'days').toDate(),
+		title: 'Sample Event 4',
+	},
+	{
+		start: moment(new Date()).add(4, 'days').toDate(),
+		end: moment(new Date()).add(4, 'days').toDate(),
+		title: 'Sample Event 5',
+	},
 ];
 
 const AgendaCalendar = () => {
 	return (
-		<div>
+		<>
 			<Calendar
+				selectable
 				localizer={localizer}
 				events={myEventsList}
 				startAccessor="start"
 				endAccessor="end"
 				style={{ height: 500, width: 700 }}
+				defaultDate={new Date()}
+				defaultView="day"
+				views={['day']}
+				step={60}
 			/>
-		</div>
+		</>
 	);
 };
 
