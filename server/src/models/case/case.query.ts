a6d1693f0c4e644264bdee3853cbbbfd7f2f6712
rakib <rakibulislmmribd@gmail.com>
2023-08-13 12:27:24 +0600

    Add: Slots

--- server/src/models/case/case.query.ts
+++ server/src/models/case/case.query.ts
@@ -1,7 +1,8 @@
 import { CaseModel } from './case.model';
 import { Case } from '../../interfaces/case.interface';
 import { findCyclistByEmail } from '../cyclist/cyclist.query';
-import { findTechnicianByEmail } from '../technician/technician.query';
+import { findTechnicianByEmail, findTechnicianById } from '../technician/technician.query';
+import { Types } from '../database';
 
 const createNewCase = async (item: Case) => {
   return await CaseModel.create(item);
@@ -12,6 +13,18 @@ const findAllCases = async (email: string) => {
   const technician = await findTechnicianByEmail(email);
   if (cyclist && cyclist.role === 'cyclist') return await CaseModel.find({ cyclist: cyclist?._id });
   else return await CaseModel.find({ technician: technician?._id });
+};
+
+const findAvailableSupportTimeForTechnician = async (technicianId: string) => {
+  const cases = await CaseModel.find({ technician: technicianId });
+
+  if (cases) {
+    const subpartTime = cases.map((oneCase) => {
+      return oneCase.supportTime;
+    });
+  }
+
+  const technician = await findTechnicianById(new Types.ObjectId(technicianId));
 };
 
 const findCaseById = async (caseId: string) => {
--- server/src/models/technician/technician.model.ts
+++ server/src/models/technician/technician.model.ts
@@ -2,19 +2,19 @@ import { Technician } from '../../interfaces/technician.interface';
 import { Schema, Types, model } from '../database';
 
 const technicianSchema = new Schema({
-	name: { type: String, required: true },
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-	role: { type: String, required: true },
-	address: { type: String },
-	phone: { type: String },
-	brandsExpertise: [{ type: String }],
-	subpartExpertise: [{ type: Types.ObjectId, ref: 'SubPartModel' }],
-	workingDays: [{ type: String }],
-	workingSlots: [{ type: String }],
-	cases: [{ type: Types.ObjectId, ref: 'CaseModel' }],
-	wishlist: [{ type: String }],
-	imageUrl: { type: String },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  address: { type: String },
+  phone: { type: String },
+  brandsExpertise: [{ type: String }],
+  subpartExpertise: [{ type: Types.ObjectId, ref: 'SubPartModel' }],
+  workingDays: [{ type: String }],
+  workingSlots: [{ type: String }],
+  cases: [{ type: Types.ObjectId, ref: 'CaseModel' }],
+  wishlist: [{ type: String }],
+  imageUrl: { type: String },
 });
 
 const TechnicianModel = model<Technician>('Technician', technicianSchema);
