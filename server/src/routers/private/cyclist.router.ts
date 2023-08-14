3ad77757fb0c0d0141fdbb0f2525a2790d58108b
rakib <rakibulislmmribd@gmail.com>
2023-08-14 13:36:32 +0600

    FIX: merge conflict

--- server/src/routers/private/cyclist.router.ts
+++ server/src/routers/private/cyclist.router.ts
@@ -32,16 +32,10 @@ cyclistRouter.put('/select-plan', cyclistController.selectPlan);
 
 // bicycle
 cyclistRouter.post('/set-up-bicycle', bicycleController.setUpBicycle);
-cyclistRouter.put(
-  '/set-up-bicycle-edit/:id',
-  bicycleController.setUpBicycleEdit
-);
+cyclistRouter.put('/set-up-bicycle-edit/:id', bicycleController.setUpBicycleEdit);
 cyclistRouter.get('/bicycle-health/:id', bicycleController.getBicycleHealth);
 cyclistRouter.get('/bicycle/:id', bicycleController.getBicycle);
-cyclistRouter.get(
-  '/bicycle-damaged-part/:id',
-  bicycleController.bicycleDamagedPart
-);
+cyclistRouter.get('/bicycle-damaged-part/:id', bicycleController.bicycleDamagedPart);
 
 // order
 cyclistRouter.get('/get-plan', orderController.getPlan);
